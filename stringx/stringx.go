a8478b19018ef50cc418f25714ecea849ed13298
seven <seven>
2016-08-16 09:39:31 +0800

    stringx-valueof

--- stringx/stringx.go
+++ stringx/stringx.go
@@ -1,7 +1,16 @@
 package stringx
 
+import (
+	"strconv"
+	"strings"
+)
+
 func IsEmpty(s string) bool {
 	return len(s) == 0
+}
+
+func IsNotEmpty(s string) bool {
+	return !IsEmpty(s)
 }
 
 func CharAt() {
@@ -12,6 +21,16 @@ func Matches() {
 
 }
 
+func SplitRmEmpty(s, sep string) []string {
+	var arr []string
+	for _, a := range strings.Split(s, sep) {
+		if len(a) != 0 {
+			arr = append(arr, a)
+		}
+	}
+	return arr
+}
+
 func SubString() {
 
 }
@@ -20,7 +39,18 @@ func ToCharArray() {
 
 }
 
-func ValueOf() {
-
+func ValueOfInt(i int) string {
+	return strconv.Itoa(i)
 }
+
+
+func ValueOfFloat32(f float32) string {
+	return strconv.FormatFloat(float64(f), 'f', -1, 32)
+}
+
+func ValueOfFloat64(f float64) string {
+	return strconv.FormatFloat(f, 'f', -1, 64)
+}
+
+
 
--- stringx/stringx_test.go
+++ stringx/stringx_test.go
@@ -2,6 +2,8 @@ package stringx
 
 import (
 	"testing"
+	"strconv"
+	"strings"
 )
 
 func TestIsEmpty(t *testing.T) {
@@ -9,4 +11,19 @@ func TestIsEmpty(t *testing.T) {
 	empty := IsEmpty(s)
 
 	t.Log(empty)
+}
+
+func TestValueOfFloat(t *testing.T) {
+	var a float32
+	a = 1.23
+	f1 := ValueOfFloat32(a)
+	f2 := ValueOfFloat64(1.2)
+	t.Log(f1)
+	t.Log(f2)
+	strconv.FormatFloat(float64(a), 'f', -1, 64)
+
+	s := "1,,2,3,"
+	r := strings.SplitAfter(s, ",")
+	t.Log(r)
+
 }
