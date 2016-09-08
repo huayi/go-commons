2f77fdd3959433bc45d0ccbf8156f6d45279e0d6
huayi <zyhday@gmail.com>
2016-09-08 15:18:08 +0800

    stringx.String()

--- stringx/stringx.go
+++ stringx/stringx.go
@@ -3,6 +3,7 @@ package stringx
 import (
 	"strconv"
 	"strings"
+	"fmt"
 )
 
 func IsEmpty(s string) bool {
@@ -31,25 +32,41 @@ func SplitRmEmpty(s, sep string) []string {
 	return arr
 }
 
-func SubString() {
-
+// 截取字符串
+// 建议使用go本身的语法 str[1:5]
+func SubString(s string, start, end int) string {
+	return s[start:end]
 }
 
 func ToCharArray() {
 
 }
 
-func ValueOfInt(i int) string {
-	return strconv.Itoa(i)
-}
+func String(i interface{}) string {
 
-
-func ValueOfFloat32(f float32) string {
-	return strconv.FormatFloat(float64(f), 'f', -1, 32)
-}
-
-func ValueOfFloat64(f float64) string {
-	return strconv.FormatFloat(f, 'f', -1, 64)
+	switch s := i.(type) {
+	case string:
+		return s
+	case bool:
+		return strconv.FormatBool(s)
+	case float64:
+		return strconv.FormatFloat(i.(float64), 'f', -1, 64)
+	case int64:
+		return strconv.FormatInt(i.(int64), 10)
+	case int:
+		return strconv.FormatInt(int64(i.(int)), 10)
+	case []byte:
+		return string(s)
+	case nil:
+		return ""
+	case fmt.Stringer:
+		return s.String()
+	case error:
+		return s.Error()
+	default:
+		fmt.Errorf("Unable to Cast %#v to string", i)
+		return ""
+	}
 }
 
 
--- stringx/stringx_test.go
+++ stringx/stringx_test.go
@@ -1,9 +1,9 @@
 package stringx
 
 import (
-	"testing"
 	"strconv"
-	"strings"
+	"testing"
+	//"strings"
 )
 
 func TestIsEmpty(t *testing.T) {
@@ -11,6 +11,33 @@ func TestIsEmpty(t *testing.T) {
 	empty := IsEmpty(s)
 
 	t.Log(empty)
+}
+
+func TestSplitRmEmpty(t *testing.T) {
+	s := "1,2,3,"
+
+	//r := strings.SplitAfter(s, ",")
+	//t.Log(r)
+
+	//r := strings.Split(s, ",")
+	//t.Log(r)
+
+	//s = "1,2,3,"
+	r := SplitRmEmpty(s, ",")
+	t.Log(r)
+}
+
+func TestSubString(t *testing.T) {
+	s := "1234567"
+	s1 := s[:3]
+	t.Log(s1)
+
+	s2 := SubString(s, 0, 3)
+	t.Log(s2)
+
+	//var s3 string
+	t.Log(s2[0])
+	t.Log(s2[1])
 }
 
 func TestValueOfFloat(t *testing.T) {
@@ -22,8 +49,17 @@ func TestValueOfFloat(t *testing.T) {
 	t.Log(f2)
 	strconv.FormatFloat(float64(a), 'f', -1, 64)
 
-	s := "1,,2,3,"
-	r := strings.SplitAfter(s, ",")
-	t.Log(r)
+}
 
+func TestString(t *testing.T) {
+	type I int
+	var i I
+	i = 1
+	//i := "a"
+	//i := 1.25
+	//i := "a"
+	//i := "a"
+	//var i interface{}
+	s := String(i)
+	t.Log(s)
 }
